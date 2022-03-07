29c3a30d28d5074c5a4e2cc529323f973d4600af
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2022-03-07 17:31:23 +0100

    Create recipes table

--- db/migrate/20220307132046_create_recipes.rb
+++ db/migrate/20220307132046_create_recipes.rb
@@ -1,6 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
+      t.string :title
+      t.string :image
+      t.string :rating
+      t.integer :total_time
 
       t.timestamps
     end
