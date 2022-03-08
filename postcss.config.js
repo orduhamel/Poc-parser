5ffd282044404c5789e8257040b1c7f8648b9702
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2022-03-08 16:51:45 +0100

    Add and configure TailwindCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
+var tailwindcss = require('tailwindcss');
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    tailwindcss('./app/javascript/css/tailwind.js'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
